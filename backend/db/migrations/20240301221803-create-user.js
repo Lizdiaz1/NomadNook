7d242320c5298d156591ae05dccecbbba33addd1
Lizette Diaz <mdlizette1@gmail.com>
2024-03-01 19:48:30 -0500

    updates still not working

--- backend/db/migrations/20240301221803-create-user.js
+++ backend/db/migrations/20240301221803-create-user.js
@@ -6,8 +6,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+   up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -41,7 +41,7 @@ module.exports = {
     }, options);
   },
 
-  async down(queryInterface, Sequelize) {
+   down(queryInterface, Sequelize) {
     options.tableName = "Users";
     return queryInterface.dropTable(options);
   }

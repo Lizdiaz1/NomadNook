d4e6e8136905d3c9110e3f9a277eab55e9f086b3
Lizette Diaz <mdlizette1@gmail.com>
2024-03-05 18:43:56 -0500

    updates

--- backend/db/seeders/20240116062812-demo-image.js
+++ backend/db/seeders/20240116062812-demo-image.js
@@ -12,56 +12,56 @@ module.exports = {
       // options,
       [
         {
-          imageableId: 1,
-          imageableType: 'Spot',
+          spotId: 1,
+          Type: 'Spot',
           preview: true,
           url: "https://i.pinimg.com/564x/d5/4d/5e/d54d5e6bf5beca90f058d1a512fa27df.jpg"
         },
         {
-          imageableId: 2,
-          imageableType: 'Spot',
+          spotId: 2,
+          Type: 'Spot',
           preview: true,
           url: 'https://media-cdn.tripadvisor.com/media/photo-s/11/84/36/46/desert-tent.jpg'
         },
         {
-          imageableId: 3,
-          imageableType: 'Spot',
+          spotId: 3,
+          Type: 'Spot',
           preview: true,
           url: "https://na.rdcpix.com/1821951854/b57cb3fe060b4e365f4756e99b2b4287w-c249155rd-w832_h468_r4_q80.jpg"
         },
         {
-          imageableId: 4,
-          imageableType: 'Spot',
+          spotId: 4,
+          Type: 'Spot',
           preview: true,
           url: "https://eb2pgoq5kpf.exactdn.com/wp-content/uploads/2015/04/HoneyWereHomeMasterBedroom.134.jpg?strip=all&lossy=1&ssl=1"
         },
         {
-          imageableId: 5,
-          imageableType: 'Spot',
+          spotId: 5,
+          Type: 'Spot',
           preview: true,
           url: "https://png.pngtree.com/thumb_back/fw800/background/20230722/pngtree-opulent-purple-bedroom-featuring-lavish-furnishings-ornate-golden-patterns-a-spacious-image_3784783.jpg"
         },
         {
-          imageableId: 1,
-          imageableType: 'Review',
+          spotId: 1,
+          Type: 'Review',
           preview: true,
           url: "https://media.tenor.com/MFE6UiMEpRoAAAAC/math-zack-galifianakis.gif"
         },
         {
-          imageableId: 2,
-          imageableType: 'Review',
+          spotId: 2,
+          Type: 'Review',
           preview: true,
           url: "https://media.tenor.com/MFE6UiMEpRoAAAAC/math-zack-galifianakis.gif"
         },
         {
-          imageableId: 3,
-          imageableType: 'Review',
+          spotId: 3,
+          Type: 'Review',
           preview: true,
           url: "https://media.tenor.com/MFE6UiMEpRoAAAAC/math-zack-galifianakis.gif"
         },
         {
-          imageableId: 4,
-          imageableType: 'Review',
+          spotId: 4,
+          Type: 'Review',
           preview: true,
           url: "https://media.tenor.com/MFE6UiMEpRoAAAAC/math-zack-galifianakis.gif"
         },
@@ -76,7 +76,7 @@ module.exports = {
     return queryInterface.bulkDelete(
       options,
       {
-        imageableId: { [Op.in]: [1, 2, 3, 4] },
+        spotId: { [Op.in]: [1, 2, 3, 4] },
       },
       {}
     );

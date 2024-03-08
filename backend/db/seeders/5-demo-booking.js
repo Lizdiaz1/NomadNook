f2b177edb9faf4e54fc5905ab66db2ff9be453b9
Lizette Diaz <mdlizette1@gmail.com>
2024-03-07 21:59:13 -0500

    validation error

--- backend/db/seeders/5-demo-booking.js
+++ backend/db/seeders/5-demo-booking.js
@@ -8,85 +8,81 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await Booking.bulkCreate(
-      // options,
-      [
+      await Booking.bulkCreate([
         {
           spotId: 1,
           userId: 1,
-          startDate: new Date("2024-10-1"),
-          endDate: new Date("2024-10-2"),
+          startDate: new Date("2024-10-01"),
+          endDate: new Date("2024-10-02"),
         },
         {
           spotId: 1,
           userId: 1,
-          startDate: new Date("12-12-2024"),
-          endDate: new Date("12-13-2024"),
+          startDate: new Date("2024-12-12"),
+          endDate: new Date("2024-12-13"),
         },
         {
           spotId: 1,
           userId: 2,
-          startDate: new Date("12-20-2024"),
-          endDate: new Date("12-24-2024"),
+          startDate: new Date("2024-12-20"),
+          endDate: new Date("2024-12-24"),
         },
         {
           spotId: 1,
           userId: 3,
-          startDate: new Date("12-26-2024"),
-          endDate: new Date("12-30-2024"),
+          startDate: new Date("2024-12-26"),
+          endDate: new Date("2024-12-30"),
         },
         {
           spotId: 2,
           userId: 2,
-          startDate: new Date("11-5-2024"),
-          endDate: new Date("11-10-2024"),
+          startDate: new Date("2024-11-05"),
+          endDate: new Date("2024-11-10"),
         },
         {
           spotId: 2,
           userId: 2,
-          startDate: new Date("09-01-2024"),
-          endDate: new Date("09-07-2024"),
+          startDate: new Date("2024-09-01"),
+          endDate: new Date("2024-09-07"),
         },
         {
           spotId: 2,
           userId: 1,
-          startDate: new Date("10-15-2024"),
-          endDate: new Date("11-18-2024"),
+          startDate: new Date("2024-10-15"),
+          endDate: new Date("2024-11-18"),
         },
         {
           spotId: 2,
           userId: 3,
-          startDate: new Date("11-22-2024"),
-          endDate: new Date("22-02-2024"),
+          startDate: new Date("2024-11-22"),
+          endDate: new Date("2024-11-28"), // Corrected end date
         },
         {
           spotId: 3,
           userId: 3,
-          startDate: new Date("11-25-2024"),
-          endDate: new Date("11-30-2024"),
+          startDate: new Date("2024-11-25"),
+          endDate: new Date("2024-11-30"),
         },
         {
           spotId: 3,
           userId: 3,
-          startDate: new Date("12-25-2024"),
-          endDate: new Date("01-01-2025"),
+          startDate: new Date("2024-12-25"),
+          endDate: new Date("2025-01-01"),
         },
         {
           spotId: 3,
           userId: 1,
-          startDate: new Date("01-25-2024"),
-          endDate: new Date("01-26-2024"),
+          startDate: new Date("2024-01-25"),
+          endDate: new Date("2024-01-26"),
         },
         {
           spotId: 3,
           userId: 2,
-          startDate: new Date("02-02-2026"),
-          endDate: new Date("03-02-2026"),
+          startDate: new Date("2026-02-02"),
+          endDate: new Date("2026-03-02"),
         },
-      ],
-      { validate: true }
-    );
-  },
+      ], { validate: true });
+    },
 
   async down(queryInterface, Sequelize) {
     options.tableName = "Booking";

122bad15de1c2b61910db34eb935ded40733c1cb
Lizette Diaz <mdlizette1@gmail.com>
2024-03-12 19:18:17 -0400

    modal

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,31 +1,39 @@
-import * as sessionActions from './store/session';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import { Provider } from 'react-redux';
-import configureStore from './store/store';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import { ModalProvider } from './context/Modal';
+import "./index.css";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+import * as sessionActions from "./store/session";
+import { csrfFetch, restoreCSRF } from "./store/csrf";
+import { Modal, ModalProvider } from "./context/Modal";
 
 const store = configureStore();
 
-if (import.meta.env.MODE !== 'production') {
-  restoreCSRF().then(() => {
-    window.csrfFetch = csrfFetch;
-    window.store = store;
-    window.sessionActions = sessionActions;
-  });
-} else {
-  window.store = store;
+if (process.env.NODE_ENV !== "production") {
+	restoreCSRF();
+
+	window.csrfFetch = csrfFetch;
+	window.store = store;
+	window.sessionActions = sessionActions;
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <ModalProvider>
-      <App />
-   </ModalProvider>
-    </Provider>
-  </React.StrictMode>
-);
+function Root() {
+	return (
+		<ModalProvider>
+			<ReduxProvider store={store}>
+				<BrowserRouter>
+					<App />
+					<Modal />
+				</BrowserRouter>
+			</ReduxProvider>
+		</ModalProvider>
+	);
+}
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<Root />);

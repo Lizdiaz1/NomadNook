2cd0cb8199d96dccf3ec2ce0cdd8c1a980f827cb
Lizette Diaz <mdlizette1@gmail.com>
2024-04-14 04:40:38 -0400

    updated index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
@@ -15,5 +14,21 @@ export default defineConfig(({ mode }) => ({
     proxy: {
       '/api': 'http://localhost:8000'
     },
+  },
+  css: {
+    // This option is not necessary by default, but adding for explicit configuration
+    modules: {
+      scopeBehaviour: 'local'
+    },
+    preprocessorOptions: {
+      css: {
+        javascriptEnabled: true // For CSS modules that might need JS evaluation
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/frontend/src/', // Adjust this path as necessary
+    }
   }
 }));

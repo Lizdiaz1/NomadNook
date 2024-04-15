5451e7dc35afbc851be545a26ed34bede5c4bce5
Lizette Diaz <mdlizette1@gmail.com>
2024-04-15 16:52:33 -0400

    Updates to eslintrc.cjs & package.json

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+      node: true,
+      es2021: true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module'
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    }
+  }
--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,20 +1,40 @@
 module.exports = {
+  overrides: [
+    {
+      files: [
+        "src/context/*.jsx",
+        "backend/utils/*.js"  
+      ],
+      rules: {
+        'react-refresh/only-export-components': 'off'
+      }
+    }
+  ],
   root: true,
-  env: { browser: true, es2020: true, node: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: { version: '18.2' }
+  },
   plugins: ['react-refresh'],
-  ignorePatterns: ['dist', 'node_modules'],
+  ignorePatterns: ['dist', 'node_modules'],  // Make sure backend is not being ignored
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
     'react/prop-types': 'off'
   },

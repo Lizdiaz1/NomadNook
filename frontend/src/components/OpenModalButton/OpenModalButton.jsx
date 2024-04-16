5158383b94da1c39d2f739362c6196dc3f53a3d6
Lizette Diaz <mdlizette1@gmail.com>
2024-04-15 20:20:44 -0400

    Zaviar Changeses

--- frontend/src/components/OpenModalButton/OpenModalButton.jsx
+++ frontend/src/components/OpenModalButton/OpenModalButton.jsx
@@ -6,7 +6,7 @@ function OpenModalButton({
   buttonText, // text of the button that opens the modal
   onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
   onModalClose, // optional: callback function that will be called once the modal is closed
-  buttonClassName
+  buttonClassName 
 }) {
   const { setModalContent, setOnModalClose } = useModal();
 

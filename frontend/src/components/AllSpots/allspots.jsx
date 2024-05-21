72746f81e8e1f7b8563528ee1dd0ebf98cbb7b04
Lizette Diaz <mdlizette1@gmail.com>
2024-05-21 08:24:00 -0400

    allspots & spots updates

--- frontend/src/components/AllSpots/allspots.jsx
+++ frontend/src/components/AllSpots/allspots.jsx
@@ -4,7 +4,7 @@ import './AllSpots.css';
 import { getSpots } from '../../store/spots.js';
 import { NavLink } from 'react-router-dom';
 
-function AllSpots  () {
+function AllSpots() {
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -12,19 +12,18 @@ function AllSpots  () {
         console.log('Dispatched getSpots');
     }, [dispatch]);
 
-    // const spots = useSelector((state) => {
-    //     const allSpots = state.spots.list.map((spotId) => state.spots[spotId]);
-    //     console.log('All spots:', allSpots);
-    //     return allSpots;
-    // });
     const spots = useSelector((state) => {
-        return state.spots.list.map((spotId) => state.spots[spotId]);
+        if (!state.spots || !Array.isArray(state.spots.list)) {
+            console.error('Spots list is not an array or state.spots is undefined:', state.spots);
+            return [];
+        }
+        return state.spots.list.map((spotId) => state.spots[spotId]).filter(spot => spot);
     });
 
     console.log('Final spots:', spots);
 
     if (spots.length === 0) {
-        return null; // Consider a possible loading indicator or a message
+        return <div>Loading spots...</div>; // Consider a possible loading indicator or a message
     }
 
     const getDecimal = (num) => {
@@ -34,48 +33,137 @@ function AllSpots  () {
     return (
         <div className='outer-nav-container'>
             <nav>
-                {[...spots].reverse().map((spot) => (
-                    <div key={spot.id} className='all-spots'>
-                        <NavLink to={`/spots/${spot.id}`}>
-                            <div className='thumbnail-container'>
-                                <img
-                                    src={`${spot.previewImage}`}
-                                    alt='Spot Preview Image'
-                                    className='thumbnail-image'
-                                    title={`${spot.name}`}
-                                />
-                                <div className='thumbnail-info'>
-                                    <div className='secondary-text'>
-                                        {spot.avgRating === 0 ? (
-                                            <>
-                                                <div className='address-text'>{`${spot.city}, ${spot.state}`}</div>
-                                                <div className='star-text'>
-                                                    <i className='fa-solid fa-star'></i> New
-                                                </div>
-                                                <div className='price-text'>
-                                                    <b>${spot.price}</b> night
-                                                </div>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <div className='address-text'>{`${spot.city}, ${spot.state}`}</div>
-                                                <div className='star-text'>
-                                                    <i className='fa-solid fa-star'></i> {getDecimal(spot.avgRating)}
-                                                </div>
-                                                <div className='price-text'>
-                                                    <b>${spot.price}</b> night
-                                                </div>
-                                            </>
-                                        )}
+                {[...spots].reverse().map((spot) => {
+                    if (!spot || !spot.id) {
+                        console.error('Invalid spot data:', spot);
+                        return null;
+                    }
+                    return (
+                        <div key={spot.id} className='all-spots'>
+                            <NavLink to={`/spots/${spot.id}`}>
+                                <div className='thumbnail-container'>
+                                    <img
+                                        src={`${spot.previewImage}`}
+                                        alt='Spot Preview Image'
+                                        className='thumbnail-image'
+                                        title={`${spot.name}`}
+                                    />
+                                    <div className='thumbnail-info'>
+                                        <div className='secondary-text'>
+                                            {spot.avgRating === 0 ? (
+                                                <>
+                                                    <div className='address-text'>{`${spot.city}, ${spot.state}`}</div>
+                                                    <div className='star-text'>
+                                                        <i className='fa-solid fa-star'></i> New
+                                                    </div>
+                                                    <div className='price-text'>
+                                                        <b>${spot.price}</b> night
+                                                    </div>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <div className='address-text'>{`${spot.city}, ${spot.state}`}</div>
+                                                    <div className='star-text'>
+                                                        <i className='fa-solid fa-star'></i> {getDecimal(spot.avgRating)}
+                                                    </div>
+                                                    <div className='price-text'>
+                                                        <b>${spot.price}</b> night
+                                                    </div>
+                                                </>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </NavLink>
-                    </div>
-                ))}
+                            </NavLink>
+                        </div>
+                    );
+                })}
             </nav>
         </div>
     );
 }
 
 export default AllSpots;
+
+
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import './AllSpots.css';
+// import { getSpots } from '../../store/spots.js';
+// import { NavLink } from 'react-router-dom';
+
+// function AllSpots  () {
+//     const dispatch = useDispatch();
+
+//     useEffect(() => {
+//         dispatch(getSpots());
+//         console.log('Dispatched getSpots');
+//     }, [dispatch]);
+
+//     // const spots = useSelector((state) => {
+//     //     const allSpots = state.spots.list.map((spotId) => state.spots[spotId]);
+//     //     console.log('All spots:', allSpots);
+//     //     return allSpots;
+//     // });
+//     const spots = useSelector((state) => {
+//         return state.spots.list.map((spotId) => state.spots[spotId]);
+//     });
+
+//     console.log('Final spots:', spots);
+
+//     if (spots.length === 0) {
+//         return null; // Consider a possible loading indicator or a message
+//     }
+
+//     const getDecimal = (num) => {
+//         return num > 0 ? (Math.round(num * 100) / 100).toFixed(1) : 0;
+//     };
+
+//     return (
+//         <div className='outer-nav-container'>
+//             <nav>
+//                 {[...spots].reverse().map((spot) => (
+//                     <div key={spot.id} className='all-spots'>
+//                         <NavLink to={`/spots/${spot.id}`}>
+//                             <div className='thumbnail-container'>
+//                                 <img
+//                                     src={`${spot.previewImage}`}
+//                                     alt='Spot Preview Image'
+//                                     className='thumbnail-image'
+//                                     title={`${spot.name}`}
+//                                 />
+//                                 <div className='thumbnail-info'>
+//                                     <div className='secondary-text'>
+//                                         {spot.avgRating === 0 ? (
+//                                             <>
+//                                                 <div className='address-text'>{`${spot.city}, ${spot.state}`}</div>
+//                                                 <div className='star-text'>
+//                                                     <i className='fa-solid fa-star'></i> New
+//                                                 </div>
+//                                                 <div className='price-text'>
+//                                                     <b>${spot.price}</b> night
+//                                                 </div>
+//                                             </>
+//                                         ) : (
+//                                             <>
+//                                                 <div className='address-text'>{`${spot.city}, ${spot.state}`}</div>
+//                                                 <div className='star-text'>
+//                                                     <i className='fa-solid fa-star'></i> {getDecimal(spot.avgRating)}
+//                                                 </div>
+//                                                 <div className='price-text'>
+//                                                     <b>${spot.price}</b> night
+//                                                 </div>
+//                                             </>
+//                                         )}
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         </NavLink>
+//                     </div>
+//                 ))}
+//             </nav>
+//         </div>
+//     );
+// }
+
+// export default AllSpots;
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -98,6 +98,8 @@ export const deleteSpot = (spotId) => async (dispatch) => {
 // Initial State
 const initialState = {
   list: [],
+  allSpots: {},
+  currentSpot: null,
 };
 
 // Helper Function for Sorting
@@ -109,51 +111,223 @@ const sortList = (list) => {
 const spotsReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD: {
-      const allSpots = {};
-      if (Array.isArray(action.list)) {
-        action.list.map((spot) => {
+      if (Array.isArray(action.list.Spots)) {
+        const allSpots = {};
+        action.list.Spots.forEach((spot) => {
           allSpots[spot.id] = spot;
         });
         return {
           ...state,
-          ...allSpots,
-          list: sortList(action.list),
+          allSpots,
+          list: sortList(action.list.Spots),
         };
       } else {
         console.error("Action list is not an array:", action.list);
-        return state; 
+        return state;
       }
     }
 
     case CREATE_SPOT: {
-      const newState = {
+      const newSpot = action.spot;
+      return {
         ...state,
-        [action.spot.id]: action.spot,
-        list: [...state.list, action.spot.id],
+        allSpots: { ...state.allSpots, [newSpot.id]: newSpot },
+        list: sortList([...Object.values(state.allSpots), newSpot]),
       };
-      newState.list = sortList(newState.list);
-      return newState;
     }
+
     case UPDATE_SPOT: {
-      const newState = {
+      const updatedSpot = action.spot;
+      const newAllSpots = { ...state.allSpots, [updatedSpot.id]: updatedSpot };
+      return {
         ...state,
-        [action.spot.id]: action.spot,
+        allSpots: newAllSpots,
+        list: sortList(Object.values(newAllSpots)),
       };
-      newState.list = sortList(Object.values(newState));
-      return newState;
     }
+
     case GET_ONE_SPOT:
       return { ...state, currentSpot: action.payload };
 
-    case DELETE_SPOT:
-      if (state.currentSpot?.id === action.payload) {
-        const newState = { ...state, currentSpot: null };
-        return newState;
-      }
-      return state;
+    case DELETE_SPOT: {
+      // eslint-disable-next-line no-unused-vars
+      const { [action.payload]: _, ...remainingSpots } = state.allSpots;
+      return {
+        ...state,
+        allSpots: remainingSpots,
+        list: sortList(Object.values(remainingSpots)),
+        currentSpot: state.currentSpot?.id === action.payload ? null : state.currentSpot,
+      };
+    }
+
     default:
       return state;
   }
 };
 
 export default spotsReducer;
+
+
+
+
+
+// import { csrfFetch } from "./csrf";
+
+// // Action Types
+// const LOAD = "spots/LOAD";
+// const CREATE_SPOT = "spots/CREATE_SPOT";
+// const UPDATE_SPOT = "spots/UPDATE_SPOT";
+// const DELETE_SPOT = "spots/DELETE_SPOT";
+// const GET_ONE_SPOT = "spots/GET_ONE_SPOT";
+
+// // Action Creators
+// const load = (list) => ({
+//   type: LOAD,
+//   list,
+// });
+
+// const createSpot = (spot) => ({
+//   type: CREATE_SPOT,
+//   spot,
+// });
+
+// const updatedSpot = (spot) => ({
+//   type: UPDATE_SPOT,
+//   spot,
+// });
+
+// const getOneSpotAction = (spot) => ({
+//   type: GET_ONE_SPOT,
+//   payload: spot,
+// });
+
+// const removeSpotAction = (spotId) => ({
+//   type: DELETE_SPOT,
+//   payload: spotId,
+// });
+
+// // Thunk Action Creators
+// export const getSpots = () => async (dispatch) => {
+//   const response = await csrfFetch("/api/spots");
+//   if (response.ok) {
+//     const list = await response.json();
+//     dispatch(load(list));
+//   }
+// };
+
+// export const getOneSpot = (spotId) => async (dispatch) => {
+//   const response = await csrfFetch(`/api/spots/${spotId}`);
+//   if (response.ok) {
+//     const spot = await response.json();
+//     dispatch(getOneSpotAction(spot));
+//     return spot;
+//   }
+//   throw new Error("Failed to fetch spot details.");
+// };
+
+// export const createNewSpot = (spotData) => async (dispatch) => {
+//   const response = await csrfFetch("/api/spots", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(spotData),
+//   });
+//   if (response.ok) {
+//     const newSpot = await response.json();
+//     dispatch(createSpot(newSpot));
+//     return newSpot;
+//   }
+//   throw new Error("Failed to create new spot.");
+// };
+
+// export const updateExistingSpot = (spotData, spotId) => async (dispatch) => {
+//   const response = await csrfFetch(`/api/spots/${spotId}`, {
+//     method: "PUT",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(spotData),
+//   });
+//   if (response.ok) {
+//     const updatedSpotData = await response.json();
+//     dispatch(updatedSpot(updatedSpotData));
+//     return updatedSpotData;
+//   }
+// };
+
+// export const deleteSpot = (spotId) => async (dispatch) => {
+//   const response = await csrfFetch(`/api/spots/${spotId}`, {
+//     method: "DELETE",
+//   });
+
+//   if (response.ok) {
+//     dispatch(removeSpotAction(spotId));
+//     return;
+//   }
+//   throw new Error("Failed to delete spot.");
+// };
+
+// // Initial State
+// const initialState = {
+//   list: [],
+// };
+
+// // Helper Function for Sorting
+// const sortList = (list) => {
+//   return list.sort((a, b) => a.id - b.id).map((spot) => spot.id);
+// };
+
+// // Reducer
+// const spotsReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case LOAD: {
+//       const allSpots = {};
+//       if (Array.isArray(action.list)) {
+//         action.list.map((spot) => {
+//           allSpots[spot.id] = spot;
+//         });
+//         return {
+//           ...state,
+//           ...allSpots,
+//           list: sortList(action.list),
+//         };
+//       } else {
+//         // Handle the case where action.list is not an array
+//         console.error("Action list is not an array:", action.list);
+//         return state; // Return the current state as is
+//       }
+//     }
+
+//     case CREATE_SPOT: {
+//       const newState = {
+//         ...state,
+//         [action.spot.id]: action.spot,
+//         list: [...state.list, action.spot.id],
+//       };
+//       newState.list = sortList(newState.list);
+//       return newState;
+//     }
+//     case UPDATE_SPOT: {
+//       const newState = {
+//         ...state,
+//         [action.spot.id]: action.spot,
+//       };
+//       newState.list = sortList(Object.values(newState));
+//       return newState;
+//     }
+//     case GET_ONE_SPOT:
+//       return { ...state, currentSpot: action.payload };
+
+//     case DELETE_SPOT:
+//       if (state.currentSpot?.id === action.payload) {
+//         const newState = { ...state, currentSpot: null };
+//         return newState;
+//       }
+//       return state;
+//     default:
+//       return state;
+//   }
+// };
+
+// export default spotsReducer;
